2581dd8d20fe7cd1d79224ca675f9aaf155134c5
xmasterninni <soelder@hotmail.com>
2022-06-27 22:30:56 +0200

    feat: add parallell requests and request impl

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::{
     time::Duration,
 };
 
-use futures::{stream, StreamExt};
-use request::get_request;
+use futures::{future::join_all, stream, StreamExt};
 use reqwest::Client;
 use tokio::{task, time};
+
+use crate::request::MonitoringEndpoint;
 
 mod request;
 
@@ -14,20 +15,37 @@ const PARALLEL_REQUESTS: usize = 2;
 #[tokio::main]
 async fn main() {
     let client = Client::new();
+    // let urls = vec!["https://reqres.inuf/api/dsdsusers?page=2"; 2];
     let urls = vec!["https://reqres.in/api/users?page=2"; 2];
 
-    urls.iter().for_each(|url| {});
+    // let mut monitoring_endpoint = MonitoringEndpoint::new(client.clone(), urls[0].to_string());
+    // monitoring_endpoint.get_request().await;
 
-    // let forever = tokio::spawn(async move {
-    //     let mut interval = time::interval(Duration::from_secs(5));
+    // if monitoring_endpoint.error().await.is_none() {
+    //     let status = monitoring_endpoint.status().await;
+    //     println!("{:?}", status)
+    // } else {
+    //     println!("{:?}", monitoring_endpoint.error().await.unwrap())
+    // }
 
-    //     loop {
-    //         let cloned_client = client.clone();
-    //         interval.tick().await;
-    //         let request = get_request(cloned_client, "https://reqres.in/api/users?page=2").await;
-    //         println!("{:?}", request.status());
-    //     }
-    // });
+    let handles = urls.into_iter().map(|url| {
+        let cloned_client = client.clone();
+        return tokio::spawn(async move {
+            let mut interval = time::interval(Duration::from_secs(5));
+            let mut monitoring_endpoint = MonitoringEndpoint::new(cloned_client, url.to_string());
 
-    // forever.await.unwrap();
+            loop {
+                interval.tick().await;
+                monitoring_endpoint.get_request().await;
+                if monitoring_endpoint.error().await.is_none() {
+                    let status = monitoring_endpoint.status().await;
+                    println!("{:?}", status)
+                } else {
+                    println!("{:?}", monitoring_endpoint.error().await.unwrap())
+                }
+            }
+        });
+    });
+
+    join_all(handles).await;
 }
--- src/request.rs
+++ src/request.rs
@@ -1,16 +1,46 @@
-use std::sync::{Arc, Mutex};
+use reqwest::{Client, Error, Response, StatusCode};
 
-use reqwest::{Client, Response, StatusCode};
-
-pub async fn get_request(client: Client, url: &str) -> Response {
-    let response = client.get(url).send().await.unwrap();
-    response
-    // assert_eq!(response.status(), StatusCode::OK);
+pub struct MonitoringEndpoint {
+    client: Client,
+    url: String,
+    error: Option<Error>,
+    last_response: Option<Response>,
 }
 
-// pub async fn post_request_with_body(url: &str, body: &str) -> Response {
-//     let client = Client::new();
-//     let response = client.post(url).body(body).send().await.unwrap();
-//     response
-//     // assert_eq!(response.status(), StatusCode::OK);
-// }
+impl MonitoringEndpoint {
+    pub fn new(client: Client, url: String) -> MonitoringEndpoint {
+        return MonitoringEndpoint {
+            client,
+            url,
+            error: None,
+            last_response: None,
+        };
+    }
+
+    pub async fn get_request(&mut self) {
+        let response = self.client.get(&self.url).send().await;
+
+        match response {
+            Ok(res) => {
+                self.error = None;
+                self.last_response = Some(res);
+            }
+            Err(err) => {
+                self.error = Some(err);
+                self.last_response = None;
+            }
+        }
+    }
+
+    pub async fn status(&self) -> Option<StatusCode> {
+        if self.last_response.is_some() {
+            return Some(self.last_response.as_ref().unwrap().status());
+        } else {
+            return None;
+        }
+    }
+
+    pub async fn error(&self) -> Option<&Error> {
+        return self.error.as_ref();
+    }
+}
